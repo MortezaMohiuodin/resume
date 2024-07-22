4adf0032338de17b623957c7f7e8e19f653ea7ea
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-23 01:17:57 +0330

    add theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,58 @@
+import { useState } from "react"
 function Header(){
+    const [show , setShow] = useState(false)
     return (
         <>
            <header className="l-header" id="header">
             <nav className="nav bd-container">
                 <a href="#" className="nav_logo">Léa GALLIER</a>
-                <div className="nav_menu" id="nav-menu">
+                <div className={`nav_menu ${show && 'show-menu'}`} id="nav-menu">
                     <ul className="nav_list">
 
                         <li className="nav_item">
-                            <a href="#home" className="nav_link active-link">
+                            <a href="#home" className="nav_link active-link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-house nav_icon"></i>Home
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#profile" className="nav_link">
+                            <a href="#profile" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-user nav_icon"></i>Profile
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#skills" className="nav_link">
+                            <a href="#skills" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-computer nav_icon"></i>Skills
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#languages" className="nav_link">
+                            <a href="#languages" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-comments nav_icon"></i>Languages
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#experience" className="nav_link">
+                            <a href="#experience" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-briefcase nav_icon"></i>Experiences
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#certificates" className="nav_link">
+                            <a href="#certificates" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-graduation-cap nav_icon"></i>Certificates
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#education" className="nav_link">
+                            <a href="#education" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-book-bookmark nav_icon"></i>Education
                             </a>
                         </li>
 
                         <li className="nav_item">
-                            <a href="#interests" className="nav_link">
+                            <a href="#interests" className="nav_link" onClick={()=>setShow(false)}>
                                 <i className="fa-solid fa-icons nav_icon"></i>Interests
                             </a>
                         </li>
@@ -58,7 +60,7 @@ function Header(){
                     </ul>
                 </div>
 
-                <div className="nav_toggle" id="nav-toggle">
+                <div className="nav_toggle" id="nav-toggle" onClick={()=>setShow(!show)}>
                     <i className="fa-solid fa-bars"></i>
                 </div>
             </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import html2pdf from 'html2pdf.js'
 import profile from '../assets/profile.png'
 function Home(){
+    const defaultState = localStorage.getItem("dark-mode");
+    const [theme,setTheme] = useState(defaultState)
+    const generatePdf = ()=>{
+          // Adapt the area of the PDF
+        
+        addScaleCV();
+        // Generate the PDF
+        generateResume();
+        // Remove adaptation after 1 second (you can choose to set more than 1 second if your PDF download time is long)
+        setTimeout(removeScaleCV, 1000);
+    }
+    function enableDarkMode() {
+        setTheme('dark')
+        document.body.classList.add('dark-theme');
+        localStorage.setItem("dark-mode", "enabled");
+      }
+      
+      function disableDarkMode() {
+        setTheme('light')
+        document.body.classList.remove('dark-theme');
+        localStorage.setItem("dark-mode", "disabled");
+      }
+    function handleThemeChange (){
+        theme === 'dark' ? disableDarkMode() : enableDarkMode() 
+    }
     return (
         <>
           <section className="home" id="home">
@@ -31,12 +58,36 @@ function Home(){
                             </div>
                         </div>
 
-                        <i className="fa-solid fa-moon change-theme" title="Theme" id="theme-button"></i>
+                        <i className={`fa-solid fa-moon change-theme ${theme === 'dark' ? 'fa-moon' : 'fa-sun'}`} title="Theme" id="theme-button" onClick={()=>handleThemeChange()}></i>
 
-                        <i className="fa-solid fa-download generate-pdf" title="Generate PDF" id="resume-button"></i>
+                        <i className="fa-solid fa-download generate-pdf" title="Generate PDF" id="resume-button" onClick={()=>generatePdf()}></i>
 
                     </section>
         </>
     )
 }
 export default Home
+
+function addScaleCV() {
+    document.body.classList.add("scale-cv");
+}
+
+// Generate PDF with html2pdf.js
+function generateResume() {
+    // PDF filename change depending of the light/dark mode
+    // PDF generated area
+    let areaCV = document.getElementById("area-cv");
+    let opt = {
+        margin: 0,
+        filename: "myResumeCV-light.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 4, useCORS: true },
+        jsPDF: { format: "a4", orientation: "portrait" },
+    };
+    html2pdf(areaCV, opt);
+}
+
+function removeScaleCV() {
+    document.body.classList.remove("scale-cv");
+}
+
