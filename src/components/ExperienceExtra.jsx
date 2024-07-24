4c8b5191c4a6c686e6a0d52c90c23929a66aa54a
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-24 18:08:18 +0330

    add

--- src/components/ExperienceExtra.jsx
+++ src/components/ExperienceExtra.jsx
@@ -6,14 +6,12 @@ function Experience(){
 
                         <div className="experience_container bd-grid">
                             <div className="experience_content">
-                            <div className="experience_time">
-                                    <span className="experience_rounder"></span>
+                                <div className="experience_time">
                                     <span className="experience_line"></span>
+                                    <span className="experience_rounder"></span>
+
                                 </div>
                                 <div className="experience_data bd-grid">
-                                    <h3 className="experience_title">Frontend Developer</h3>
-                                    <span className="experience_company">Pioneer Games | Tehran</span>
-                                    <span className="experience_year">July 2021 , Jan 2022</span>
                                     <p className="experience_description">
                                     &#8226;   Developed cryptocurrency exchange website and user dashboard with Next , Redux , GraphQL and MUI.
                                     </p>
@@ -24,38 +22,7 @@ function Experience(){
                                     </p>
                                 </div>
                             </div>
-                            <div className="experience_content">
-                                <div className="experience_time">
-                                    <span className="experience_rounder"></span>
-                                    <span className="experience_line"></span>
-                                </div>
-                                <div className="experience_data bd-grid">
-                                    <h3 className="experience_title">Frontend Developer</h3>
-                                    <span className="experience_company">Mom Fertility Center | Tehran</span>
-                                    <span className="experience_year">July 2020 , Apr 2021</span>
-                                    <p className="experience_description">
-                                    &#8226;    Designed and implemented CMS UI for the main website.
-                                    </p>
-                                    <p className="experience_description">
-                                    &#8226;   
-                                        Refactored components for improved reusability, reducing repetitive code by 50%.
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="experience_content">
-                                <div className="experience_time">
-                                    <span className="experience_rounder"></span>
-                                </div>
-                                <div className="experience_data bd-grid">
-                                    <h3 className="experience_title">Intern Frontend Developer</h3>
-                                    <span className="experience_company">Signal Co | Qom</span>
-                                    <span className="experience_year">May 2018 , May 2020</span>
-                                    <p className="experience_description">
-                                        &#8226; Developed UI components using React with MVVM architecture.
-                                    </p>
-                                    <p className="experience_description">&#8226; Collaborated on resolving critical SEO issues and optimizing performance.</p>
-                                </div>
-                            </div>
+                          
                         </div>
                     </section>
         </>
