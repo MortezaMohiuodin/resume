48ac2e9773cecd826313ad764b748af870fd2dcc
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-25 13:53:29 +0330

    add

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,16 +1,18 @@
-function Profile(){
+function Profile() {
     return (
         <>
-              <section className="profile section" id="profile">
-                        <h2 className="section-title">About Me</h2>
+            <section className="profile section" id="profile">
+                <h2 className="section-title">About Me</h2>
 
-                        <p className="profile_description">
-                        I am a front-end developere eager to apply my knowledge and specialized skills in a dynamic and challenging team.<br/>
+                <p className="profile_description">
+                    skilled and motivated ...
+                    Seeking a front-end developer position with a company which will reqiure me to utilize my skills, abilities and experiance in the IT field to ensure the company's success.
+                    I am a front-end developere eager to apply my knowledge and specialized skills in a dynamic and challenging team.<br />
 
-                        I mainly use Vue , Nuxt and related ecosystem tools to create robust , user-friendly softwares.
-
-                        </p>
-                    </section>
+                    I mainly use Vue , Nuxt and related ecosystem tools to create robust , user-friendly softwares.
+                    I've honed my skills through specialized courses, personal projects, and working as a frontend developer at a software development company. Currenty I'm focused on ....
+                </p>
+            </section>
         </>
     )
 }
